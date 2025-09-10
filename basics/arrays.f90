9c043b271d4b4f012d813f6f2df0393665cc976a
Lexa <martin.hurtis@outlook.sk>
2025-09-10 22:19:20 +0200

    add more way to construct and print 1D array

--- basics/arrays.f90
+++ basics/arrays.f90
@@ -1,28 +1,48 @@
  program arrays
-
     implicit none
-    
+    ! 1D integer array
     integer, dimension(15) :: onedim
-    integer, dimension(10) :: resultarray
+    integer :: resultarray(10)
+    ! 2D integer array
     integer, dimension(10, 10) :: twodim
-    integer :: i, j
+    ! 1D real array
     real, dimension(5) :: realarray
+
     real :: randomReal
+    integer :: i, j
+
     print *, "Start"
+    ! initialize first indice of array
     onedim(1) = 999
-
     print *, onedim(1)
-    print *, twodim(4,8)
-
+   
     ! Zero out the array
     do i=1, 15
         onedim(i) = 0
     end do
 
-    !print out the array
+    !print out the array using loop
     do i=1, 15
         write (*, '(i3)', advance = 'no') onedim(i)
     end do
+
+    onedim = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]  ! Array constructor
+    print *, ''
+    print *, onedim(1:10)  ! print out the array
+    print *, onedim(10:1:-1)  !print out the array in reverse
+
+    onedim(:) = 0  ! set all element to zero 
+    print *, "back to zero..."
+    print "(15(I5))", onedim
+
+    onedim(1:7) = 23 !set first 7 elements to 23
+    print *, "Set first 7 elements to 23"
+    print "(15(I5))", onedim
+
+    onedim = [(i, i =1, 15)]  !Implied do loop constructor
+    print *, "Back to 1 to 15.."
+    print "(15(I5))", onedim
+
 
     do i=1, 10
         do j =1, 10
